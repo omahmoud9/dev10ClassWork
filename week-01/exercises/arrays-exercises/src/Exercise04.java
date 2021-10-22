272b6bb533669757b738400c838f0aa507072937
Omar <ofm2@case.edu>
2021-10-22 15:31:47 -0400

    capsule hotel README

--- week-01/exercises/arrays-exercises/src/Exercise04.java
+++ week-01/exercises/arrays-exercises/src/Exercise04.java
@@ -4,5 +4,10 @@ public class Exercise04 {
         // 1. Declare an array to hold the names of the world's oceans.
         // Set its value using array literal notation.
         // 2. Loop over each element and print it.
+        String[] oceans = { "Atlantic", "Pacific", "Indian", "Arctic", "Antarctic"};
+        for (int i =0; i< oceans.length; i++){
+            System.out.println(oceans[i]);
+        }
+
     }
 }
